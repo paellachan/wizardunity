6cbd874449ef6da86765e81c8c6a6a7d002f5bd3
paellachan <34214370+paellachan@users.noreply.github.com>
2019-08-03 13:42:23 +0200

    first

---
+++ Assets/Naninovel/Runtime/Command/ads.cs
@@ -0,0 +1,23 @@
+﻿using Naninovel.Commands;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+[CommandAlias("ads")]
+public class ads : Command
+{
+
+    public override Task ExecuteAsync ()
+    {
+        Debug.Log("Hello World!");
+		if (Advertisement.IsReady("rewardedVideo"))
+        {
+            Advertisement.Show("rewardedVideo");
+        }
+		
+        return Task.CompletedTask;
+    }
+
+    public override Task UndoAsync () => Task.CompletedTask;
+}
