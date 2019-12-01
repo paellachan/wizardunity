1135ab8b3117b717301ba0de4e27cde133d2f042
paellachan <34214370+paellachan@users.noreply.github.com>
2019-12-01 18:31:31 +0100

    rrr

---
+++ Assets/test.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class MyCustomUI : UnityCommon.ScriptableUIBehaviour, Naninovel.UI.IManagedUI
+{
+	public Task InitializeAsync () => Task.CompletedTask;
+}
+ 
